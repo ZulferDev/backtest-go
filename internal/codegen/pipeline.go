22fc13d8581ed4ded088e1d07c37794227b42495
ZulferDev <205324118+ZulferDev@users.noreply.github.com>
2026-08-29 15:46:50 +0000

    feat(phase3.1): add missing pipeline.go

---
+++ internal/codegen/pipeline.go
@@ -0,0 +1,40 @@
+package codegen
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/ZulferDev/backtest-go/internal/validator"
+)
+
+type Pipeline struct {
+	WorkDir string
+}
+
+func NewPipeline(workDir string) *Pipeline {
+	return &Pipeline{WorkDir: workDir}
+}
+
+func (p *Pipeline) Validate(code, filename string) ([]validator.ValidationError, error) {
+	return validator.ValidateStrategy(filename, []byte(code))
+}
+
+func (p *Pipeline) SaveCode(code, filename string) (string, error) {
+	dir := filepath.Join(p.WorkDir, "strategies")
+	os.MkdirAll(dir, 0755)
+	path := filepath.Join(dir, filename)
+	err := os.WriteFile(path, []byte(code), 0644)
+	return path, err
+}
+
+func (p *Pipeline) Compile(path string) error {
+	cmd := exec.Command("go", "build", path)
+	cmd.Dir = p.WorkDir
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("compile failed: %s", output)
+	}
+	return nil
+}
